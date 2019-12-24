78c998ad0b5c6797384bba62b18a2fbd48b906c0
roden1978 <rdn.homebox@gmail.com>
2019-12-24 17:01:19 +0500

    модальное окно с фото закрывается по клику на фото

--- src/common/ModalPhoto.jsx
+++ src/common/ModalPhoto.jsx
@@ -3,6 +3,7 @@ import {makeStyles} from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import ButtonBase from "@material-ui/core/ButtonBase";
 
 const useStyles = makeStyles(theme => ({
     modal: {
@@ -52,7 +53,12 @@ export default function TransitionsModal(props) {
             >
                 <Fade in={open}>
                     <div className={classes.paper}>
-                        <img className={classes.im} src={props.url} alt=''/>
+                        <ButtonBase   onClick={()=>{
+                            handleClose();
+                        }}>
+                            <img className={classes.im} src={props.url} alt=''/>
+                        </ButtonBase>
+
                     </div>
                 </Fade>
             </Modal>
