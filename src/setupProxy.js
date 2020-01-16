cdfec216fb996c938cc1b6f090c5ebc167340e02
roden1978 <rdn.homebox@gmail.com>
2020-01-16 13:23:52 +0500

    deploy heroku 03

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,10 @@
-//const proxy = require("http-proxy-middleware");
-import proxy from 'http-proxy-middleware'
-export const app = () =>{
-    app.use(
-        proxy(["/api", , "/otherApi"], { target: "https://warm-eyrie-80483.herokuapp.com" })
-    )
-}
-/*
+const proxy = require("http-proxy-middleware");
+
 module.exports = function(app) {
     app.use(
-        proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
+        proxy(["/api", , "/otherApi"], { target: "https://warm-eyrie-80483.herokuapp.com" })
     );
 };
-"proxy": "https://warm-eyrie-80483.herokuapp.com",
-*/
+
+/*"proxy": "https://warm-eyrie-80483.herokuapp.com",*/
+
