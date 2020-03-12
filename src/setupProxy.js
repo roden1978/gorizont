ae83d68408c675a885bf24cf4c69fae29e4f30b1
roden1978 <rdn.homebox@gmail.com>
2020-03-12 13:31:11 +0500

    inet deploy 03

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
     app.use(
         proxy('/api', {
-            target: "https://warm-eyrie-80483.herokuapp.com",
+            target: "http://194.58.119.89:5000",
             changeOrigin: true,
             secure: false
         })
